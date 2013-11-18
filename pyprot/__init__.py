159155ed1cd8acf80e747ed829d228e8eec52c0c
rasbt <raskberry@mail.com>
2013-11-18 13:34:22 -0500

    class inheritance

--- pyprot/__init__.py
+++ pyprot/__init__.py
@@ -1,5 +1,8 @@
-"""PyProt makes working with protein files very convenient. It supports popular protein structure file formats such as PDB and MOL2."""
+"""PyProt makes working with protein files very convenient. 
+It supports popular protein structure file formats such as PDB and MOL2."""
 
-from pyprot.pdbfile import PdbFile
+from pyprot.pdbobj import PdbObj
+from pyprot.pdbmanip import PdbManip
+from pyprot.pdbstats import PdbStats
 
 __version__ = "0.0.1dev" 
