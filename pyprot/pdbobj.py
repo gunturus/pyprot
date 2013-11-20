c3a94ee00c6649038336e2d0b6ee02b488b4b8d1
rasbt <raskberry@mail.com>
2013-11-20 12:31:01 -0500

    destructor and print

--- pyprot/pdbobj.py
+++ pyprot/pdbobj.py
@@ -1,5 +1,7 @@
 # Sebastian Raschka 11/05/2013
 #
+import os
+
 from . import pdbmanip
 from . import pdbstats
 
@@ -11,9 +13,18 @@ class PdbObj(pdbmanip.PdbManip, pdbstats.PdbStats):
         self.code = pdb_code
         self.atom = []
         self.hetatm = []
-    
-    def __str__(self):
-        print_cont = "\nPDB Code: {}\n".format(self.code())
-        for line in self.cont:
+
+    def __del__(self):
+        del self
+
+    def __repr__(self):
+        print_cont = "\nPDB Code: {}\n".format(self.code)
+        for line in self.cont[:5]:
             print_cont += "\n{}".format(line)
+        print_cont += "\n  ..."
         return print_cont
+
+
+    def __str__(self):
+        return self.__repr__()
+
