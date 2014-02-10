ff7ec8b414b69bc7f376b800aeb77541b7e43cda
Sebastian Raschka <raschkas@msu.edu>
2014-02-10 01:33:06 -0500

    cleaning up submodulde __init__ files

--- pyprot/mol2/__init__.py
+++ pyprot/mol2/__init__.py
@@ -1,4 +1,1 @@
-from pyprot.mol2.mol2_io import split_multimol2
-from pyprot.mol2.mol2_filter import contains_atom
-from pyprot.mol2.mol2_filter import distance_match
-from pyprot.mol2.mol2_filter import mol2_to_coords
+
--- pyprot/pdb/__init__.py
+++
@@ -1,3 +0,0 @@
-from pyprot.pdb.pdbobj import PdbObj
-from pyprot.pdb.pdbmanip import PdbManip
-from pyprot.pdb.pdbstats import PdbStats
