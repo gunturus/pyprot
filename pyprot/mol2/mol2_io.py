602397e586eaef42865fabfd6f17d8bcfde4b5f4
Sebastian Raschka <se.raschka@me.com>
2014-02-10 12:22:40 -0500

    added generator support

--- pyprot/mol2/mol2_io.py
+++ pyprot/mol2/mol2_io.py
@@ -6,11 +6,13 @@ def split_multimol2(multimol2):
 
     Arguments:
         multimol2 (string): path to the multi-mol2 file
+        generator (bool): If True, returns a generator instead of a list.
 
     Returns:
         A list consisting of a sublist for every extracted mol2-file. Sublists contain
         the molecule ID and the mol2 file contents.
         e.g., [['ID1234', '@<TRIPOS>MOLECULE...'],['ID1235', '@<TRIPOS>MOLECULE...'], ...]
+        If generator=True, a generator for the list items is returned instead.
     
     """
     with open(multimol2, 'r') as mol2file:
@@ -32,6 +34,10 @@ def split_multimol2(multimol2):
                     if mol2file.tell() == os.fstat(mol2file.fileno()).st_size:
                         mol2cont += line
                         break
-                
-                single_mol2s.append([molecule_id, mol2cont])
-    return single_mol2s
+                if not generator:
+                    single_mol2s.append([molecule_id, mol2cont])
+                else:
+                    yield [molecule_id, mol2cont]
+    if not generator:
+        return single_mol2s
+
